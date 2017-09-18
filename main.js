228abb3a9636cbf78801649957d4fc74e42f6707
Yang Zhang <yaaang@gmail.com>
2017-09-17 18:25:11 -0700

    Use box2d for physics

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ window.PIXI = require('phaser/build/custom/pixi');
 window.p2 = require('phaser/build/custom/p2');
 window.Phaser = require('phaser/build/custom/phaser-split');
 
+import * as Pl from 'planck-js';
+
+const ratio = 24;
+
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 const ledgeWidth = 300, ledgeHeight = 32;
@@ -24,6 +28,8 @@ var platforms;
 var cursors;
 var lava;
 var ledges = [];
+const gravity = -10;
+var world = Pl.World(Pl.Vec2(0, gravity));
 
 var stars;
 var score = 0;
@@ -33,6 +39,8 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
+
+window.dbg = {player, chars, platforms, cursors, lava, ledges, gravity, world, stars, score};
 
 class InputState {
     constructor() {
@@ -70,10 +78,26 @@ function schedRandInputs(chr) {
     setTimeout(() => schedRandInputs(chr), getRandomInt(1000, 3000));
 }
 
+function addBody(sprite, type, fixtureOpts = {}) {
+    sprite.bod = world.createBody({
+        type: type,
+        fixedRotation: true,
+        position: Pl.Vec2((sprite.x + sprite.width / 2) / ratio, -(sprite.y + sprite.height / 2) / ratio),
+        userData: sprite
+    });
+    sprite.bod.createFixture(Object.assign({
+        shape: Pl.Box(sprite.width / 2 / ratio, sprite.height / 2 / ratio),
+        density: 1,
+        restitution: 1,
+        friction: 0
+    }, fixtureOpts));
+    return sprite.bod;
+}
+
 const ledgeSpacing = 200;
 function addLedges(x) {
     while (true) {
-        if (ledges.length > 0 && ledges[ledges.length - 1].y + platforms.y - ledgeSpacing < -ledgeHeight)
+        if (ledges.length > 0 && ledges[ledges.length - 1].y - ledgeSpacing < -ledgeHeight)
             break;
         const xSpace = (game.world.width - ledgeWidth);
         const x = getRandomInt(0, xSpace / 2) + (ledges.length % 2 ? xSpace / 2 : 0);
@@ -81,15 +105,18 @@ function addLedges(x) {
             game.world.height - ledgeSpacing : ledges[ledges.length - 1].y - ledgeSpacing;
         const ledge = platforms.create(x, y, 'ground');
         ledge.scale.setTo(.75,1);
-        ledge.body.immovable = true;
+        addBody(ledge, 'kinematic');
+        ledge.bod.setLinearVelocity(Pl.Vec2(0, -2));
         ledges.push(ledge);
     }
 }
 
-function create() {
+function destroy(sprite) {
+    world.destroyBody(sprite.bod);
+    sprite.kill();
+}
 
-    //  We're going to be using physics, so enable the Arcade Physics system
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+function create() {
 
     game.world.setBounds(0,0,800,2400);
 
@@ -98,23 +125,10 @@ function create() {
 
     lava = game.add.sprite(0, game.world.height - 64, 'lava');
     lava.enableBody = true;
-    game.physics.arcade.enable(lava);
-    lava.body.immovable = true;
+    addBody(lava, 'kinematic');
 
     //  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = game.add.group();
-
-    //  We will enable physics for any object that is created in this group
-    platforms.enableBody = true;
-
-    // Here we create the ground.
-    //var ground = platforms.create(0, game.world.height - 64, 'ground');
-
-    ////  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    //ground.scale.setTo(2, 2);
-
-    ////  This stops it from falling away when you jump on it
-    //ground.body.immovable = true;
 
     //  Now let's create two ledges
     addLedges();
@@ -125,14 +139,7 @@ function create() {
         //let player = game.add.sprite(32, game.world.height - 150, 'dude');
         let player = game.add.sprite(getRandomInt(0, game.world.width), getRandomInt(0, game.world.height - 200), 'dude');
 
-        //  We need to enable physics on the player
-        game.physics.arcade.enable(player);
-
-        //  Player physics properties. Give the little guy a slight bounce.
-        player.body.bounce.x = 1;
-        player.body.bounce.y = 1;
-        player.body.gravity.y = 300;
-        player.body.collideWorldBounds = true;
+        addBody(player, 'dynamic');
 
         //  Our two animations, walking left and right.
         player.animations.add('left', [0, 1, 2, 3], 10, true);
@@ -150,20 +157,13 @@ function create() {
     //  Finally some stars to collect
     stars = game.add.group();
 
-    //  We will enable physics for any star that is created in this group
-    stars.enableBody = true;
-
     //  Here we'll create 12 of them evenly spaced apart
     for (var i = 0; i < 12; i++)
     {
         //  Create a star inside of the 'stars' group
         var star = stars.create(i * 70, 0, 'star');
 
-        //  Let gravity do its thing
-        star.body.gravity.y = 300;
-
-        //  This just gives each star a slightly random bounce value
-        star.body.bounce.y = 0.7 + Math.random() * 0.2;
+        addBody(star, 'dynamic', {restitution: 0.7 + Math.random() * 0.2});
     }
 
     //  The score
@@ -172,33 +172,63 @@ function create() {
     //  Our controls.
     cursors = game.input.keyboard.createCursorKeys();
 
+    world.on('end-contact', (contact, imp) => {
+        const fA = contact.getFixtureA(), bA = fA.getBody();
+        const fB = contact.getFixtureB(), bB = fB.getBody();
+        function bounce(fA, bA, fB, bB) {
+            if (chars.map(x => x.sprite).includes(bA.getUserData())) {
+                // only clear of each other in the next tick
+                setTimeout(() => {
+                    console.log(fA.getAABB(0).lowerBound.y, fB.getAABB(0).upperBound.y, fA.getAABB(0).upperBound.y, fB.getAABB(0).lowerBound.y);
+                    if (fA.getAABB(0).lowerBound.y >= fB.getAABB(0).upperBound.y) {
+                        bA.getLinearVelocity().y = 12;
+                    }
+                }, 0);
+            }
+        }
+        bounce(fA, bA, fB, bB);
+        bounce(fB, bB, fA, bA);
+    });
+
+    world.on('begin-contact', (contact, imp) => {
+        const fA = contact.getFixtureA(), bA = fA.getBody();
+        const fB = contact.getFixtureB(), bB = fB.getBody();
+        function bounce(fA, bA, fB, bB) {
+            if (chars.map(x => x.sprite).includes(bA.getUserData()) && stars.children.includes(bB.getUserData())) {
+                const star = bB.getUserData();
+                contact.setEnabled(false);
+                // only clear of each other in the next tick
+                setTimeout(() => {
+                    destroy(star);
+                    //  Add and update the score
+                    score += 10;
+                    scoreText.text = 'Score: ' + score;
+                }, 0);
+            }
+            if (chars.map(x => x.sprite).includes(bA.getUserData()) && lava === bB.getUserData()) {
+                contact.setEnabled(false);
+                const player = bA.getUserData();
+                // only clear of each other in the next tick
+                setTimeout(() => {
+                    destroy(player);
+                }, 0);
+            }
+        }
+        bounce(fA, bA, fB, bB);
+        bounce(fB, bB, fA, bA);
+    });
+
 }
 
-const accel = 3;
+const accel = .1;
+
+let lastTime = null;
+const dt = 1 / 60.;
 
 function update() {
 
-    function bounce (player, platform) {
-        if (player == charSprites[0])
-            console.log(player.y, player.height, player.y + player.height, platform.world.y);
-        if (player.y + player.height <= platform.world.y) {
-            console.log('above');
-            player.body.velocity.y = -350;
-        //} else if (platform.y + platform.height <= player.y) {
-        //    console.log('below');
-        //    player.body.velocity.y = -player.body.velocity.y;
-        }
-    }
-
-    function bounce2 (a, b) {
-        if (a.y + a.height <= b.y) {
-            console.log('a');
-            a.body.velocity.y = -300;
-        } else if (b.y + b.height <= a.y) {
-            console.log('b');
-            b.body.velocity.y = -300;
-        }
-    }
+    if (lastTime == null) lastTime = performance.now() / 1000;
+    const currTime = performance.now() / 1000;
 
     function die(player, lava) {
         player.kill();
@@ -206,19 +236,12 @@ function update() {
 
     const charSprites = chars.map(x => x.sprite);
 
-    //  Collide the player and the stars with the platforms
-    game.physics.arcade.collide(charSprites, platforms, bounce);
-    game.physics.arcade.collide(stars, platforms);
-    game.physics.arcade.collide(charSprites, charSprites, bounce2);
-    game.physics.arcade.collide(charSprites, lava, die);
+    world.step(dt);
 
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    game.physics.arcade.overlap(charSprites, stars, collectStar, null, this);
-
-    //console.log(charSprites.map(s => s.body.velocity.y));
-    for (let s of charSprites) {
-        s.body.velocity.y = clamp(s.body.velocity.y, 500);
-    }
+    //while (currTime - lastTime >= dt) {
+    //    world.step(dt);
+    //    lastTime += dt;
+    //}
 
     chars[0].inputs.left.isDown = cursors.left.isDown;
     chars[0].inputs.right.isDown = cursors.right.isDown;
@@ -228,10 +251,25 @@ function update() {
     for (let chr of chars) {
         feedInputs(chr);
     }
-    platforms.y += .2;
-
     addLedges();
 
+    for (var chr of charSprites) {
+        updatePos(chr);
+    }
+
+    for (let platform of platforms.children) {
+        updatePos(platform);
+    }
+
+    for (let star of stars.children) {
+        updatePos(star);
+    }
+
+}
+
+function updatePos(sprite) {
+    sprite.x = ratio * sprite.bod.getPosition().x - sprite.width / 2;
+    sprite.y = ratio * -sprite.bod.getPosition().y - sprite.height / 2;
 }
 
 function clamp(x, bound) {
@@ -246,24 +284,24 @@ function feedInputs(chr) {
     if (inputs.left.isDown)
     {
         //  Move to the left
-        player.body.velocity.x = Math.max(player.body.velocity.x - accel, -150);
+        player.bod.getLinearVelocity().x = Math.max(player.bod.getLinearVelocity().x - accel, -5);
 
         player.animations.play('left');
     }
     else if (inputs.right.isDown)
     {
         //  Move to the right
-        player.body.velocity.x = Math.min(player.body.velocity.x + accel, 150);
+        player.bod.getLinearVelocity().x = Math.min(player.bod.getLinearVelocity().x + accel, 5);
 
         player.animations.play('right');
     }
     else
     {
-        //  Reset the players velocity (movement)
-        if (player.body.velocity.x < 0) {
-            player.body.velocity.x = Math.min(0, player.body.velocity.x + accel);
+        ////  Reset the players velocity (movement)
+        if (player.bod.getLinearVelocity().x < 0) {
+            player.bod.getLinearVelocity().x = Math.min(0, player.bod.getLinearVelocity().x + accel);
         } else {
-            player.body.velocity.x = Math.max(0, player.body.velocity.x - accel);
+            player.bod.getLinearVelocity().x = Math.max(0, player.bod.getLinearVelocity().x - accel);
         }
 
         //  Stand still
@@ -271,22 +309,5 @@ function feedInputs(chr) {
 
         player.frame = 4;
     }
-    
-    //  Allow the player to jump if they are touching the ground.
-    if (inputs.up.isDown && player.body.touching.down)
-    {
-        player.body.velocity.y = -350;
-    }
-
-}
-
-function collectStar (player, star) {
-    
-    // Removes the star from the screen
-    star.kill();
-
-    //  Add and update the score
-    score += 10;
-    scoreText.text = 'Score: ' + score;
 
 }
