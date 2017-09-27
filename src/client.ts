e1de813ba58e7435234e1e56cb1f1cdab1abe528
Yang Zhang <yaaang@gmail.com>
2017-09-27 14:26:19 -0700

    Keep running after losing focus

--- src/client.ts
+++ src/client.ts
@@ -19,6 +19,7 @@ function preload() {
   game.load.image('star', 'assets/star.png');
   game.load.image('lava', 'assets/lava.jpg');
   game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+  game.stage.disableVisibilityChange = true;
 
 }
 
