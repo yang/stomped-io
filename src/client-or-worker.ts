e893782f3c22d76a8cfa97568ea4b05d902b0c2e
Yang Zhang <yaaang@gmail.com>
2017-10-11 16:58:50 -0700

    Play with webworker abstractions

--- src/client-or-worker.ts
+++ src/client-or-worker.ts
@@ -1,20 +1,42 @@
-export {};
-
 const isWebWorker = typeof WorkerGlobalScope !== 'undefined' &&
   self instanceof WorkerGlobalScope;
 
+const werk = module.exports = function werk(x, done?) {
+  throw new Error();
+}
+
 function worker() {
   self.addEventListener('message', (ev) => {
-    postMessage(ev.data);
+    postMessage(werk(ev.data));
   });
 }
 
 if (!isWebWorker) {
   const scriptPath = (<any>document.getElementById('main-script')).src;
-  const w = new Worker(scriptPath);
-  w.addEventListener('message', (ev) => {console.log(ev.data);})
-  w.postMessage('hello');
-  require("./client").main();
+
+  const threads = require('threads');
+  threads.config.set({
+    basepath: {
+      browser: 'http://localhost:8000/dist',
+      node: __dirname
+    }
+  });
+  const thread = new threads.spawn('http://localhost:8000/dist/bundle.js')
+    .send('happy')
+    .on('message', (x) => console.log(x))
+    .on('error', (x) => console.error(x))
+    .on('exit', (x) => console.log(x));
+
+  // const workerpool = require('workerpool/dist/workerpool');
+  // const pool = workerpool.pool(scriptPath);
+  // pool.exec('werk', []).then(x => console.log(x));
+
+  // const w = new Worker(scriptPath);
+  // w.addEventListener('message', (ev) => {console.log(ev.data);})
+  // w.postMessage('hello');
+  // require("./client").main();
 } else {
-  worker();
+  // const workerpool = require('workerpool/dist/workerpool');
+  // workerpool.worker({werk: werk});
+  // worker();
 }
