ba7fbd721ea886695c2f5b50c5846e4cdaf3e803
Yang Zhang <yaaang@gmail.com>
2017-10-11 17:08:21 -0700

    Demo how to force uncaught errors (for debugger)

--- src/client-or-worker.ts
+++ src/client-or-worker.ts
@@ -1,42 +1,25 @@
 const isWebWorker = typeof WorkerGlobalScope !== 'undefined' &&
   self instanceof WorkerGlobalScope;
 
-const werk = module.exports = function werk(x, done?) {
+function werk(x) {
   throw new Error();
 }
 
-function worker() {
-  self.addEventListener('message', (ev) => {
-    postMessage(werk(ev.data));
-  });
+function pRun(f) {
+  return new Promise((resolve) => setImmediate(f));
 }
 
 if (!isWebWorker) {
   const scriptPath = (<any>document.getElementById('main-script')).src;
 
-  const threads = require('threads');
-  threads.config.set({
-    basepath: {
-      browser: 'http://localhost:8000/dist',
-      node: __dirname
-    }
-  });
-  const thread = new threads.spawn('http://localhost:8000/dist/bundle.js')
-    .send('happy')
-    .on('message', (x) => console.log(x))
-    .on('error', (x) => console.error(x))
-    .on('exit', (x) => console.log(x));
+  const workerpool = require('workerpool/dist/workerpool');
+  const pool = workerpool.pool(scriptPath);
+  pool.exec('werk', [])
+    .then(x => console.log(x))
+    .catch(e => console.error(e));
 
-  // const workerpool = require('workerpool/dist/workerpool');
-  // const pool = workerpool.pool(scriptPath);
-  // pool.exec('werk', []).then(x => console.log(x));
-
-  // const w = new Worker(scriptPath);
-  // w.addEventListener('message', (ev) => {console.log(ev.data);})
-  // w.postMessage('hello');
   // require("./client").main();
 } else {
-  // const workerpool = require('workerpool/dist/workerpool');
-  // workerpool.worker({werk: werk});
-  // worker();
+  const workerpool = require('workerpool/dist/workerpool');
+  workerpool.worker({werk: () => pRun(werk)});
 }
