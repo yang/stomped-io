420d43351fbb9d3d3520d118102c5f1919bf640e
Yang Zhang <yaaang@gmail.com>
2017-10-11 22:49:00 -0700

    Demo passing instance data to web worker

--- src/client-or-worker.ts
+++ src/client-or-worker.ts
@@ -1,7 +1,10 @@
 const isWebWorker = typeof WorkerGlobalScope !== 'undefined' &&
   self instanceof WorkerGlobalScope;
 
-function werk(x) {
+// Note that the instance object passed here does not retain its class info (that would be a miracle, to be
+// resolving the correct type object in this potentially completely different environment).
+function werk(x?,y?) {
+  console.log(x.name);
   //throw new Error();
 }
 
@@ -14,12 +17,12 @@ if (!isWebWorker) {
 
   const workerpool = require('workerpool/dist/workerpool');
   const pool = workerpool.pool(scriptPath);
-  pool.exec('werk', [])
+  pool.exec('werk', [new (require('./common').Player)('z',0,0,'red'), 42])
     .then(x => console.log(x))
     .catch(e => console.error(e));
 
   require("./client").main();
 } else {
   const workerpool = require('workerpool/dist/workerpool');
-  workerpool.worker({werk: () => pRun(werk)});
+  workerpool.worker({werk: (...args) => pRun(() => werk(...args))});
 }
