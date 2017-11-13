37dc460d96c5d18ca40c81b46ec2441c6cb2c73d
Yang Zhang <yaaang@gmail.com>
2017-11-13 11:23:43 -0800

    Fix star bursts that drift forever

--- src/common.ts
+++ src/common.ts
@@ -631,7 +631,7 @@ export class Burster {
     // curr velocity = ? = last velocity / (1 - last progress) * curr progress
     const lastProgress = this.elapsed / burstDur,
       currProgress = (this.elapsed + dt) / burstDur,
-      factor = (1 - currProgress) / (1 - lastProgress);
+      factor = this.elapsed >= burstDur ? 0 : (1 - currProgress) / (1 - lastProgress);
     for (let ent of this.ents) {
       if (ent.bod.getFixtureList()) {
         if (this.elapsed > .2)
