9759ab64c05f2f4d454fb65b92e00270c7672ae5
Yang Zhang <yaaang@gmail.com>
2017-11-13 12:52:38 -0800

    Deploy private UIs/builds

--- tools/deploy.bash
+++ tools/deploy.bash
@@ -2,6 +2,7 @@ dest="$1"
 git archive --format zip -o bounce.zip master
 yarn build-prod
 rsync -ril bounce.zip $dest:
+rsync -ril build/ $dest:bounce/build/
 rsync -ril dist/ $dest:bounce/dist/
 ssh $dest '
     mkdir -p bounce;
