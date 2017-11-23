9a87e95e54d85d8652e28be483adf6de41a08a38
Yang Zhang <yaaang@gmail.com>
2017-11-22 20:34:16 -0800

    Fix main.proto in dev build
    And also disable host checking so you can use a DNS name in dev mode

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,12 +3,19 @@ const merge = require('webpack-merge');
 const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const baseConfig = require('./webpack.base.config');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = merge(baseConfig, {
   name: 'dev',
   entry: {
     'admin-client': './src/admin-client-or-worker.ts',
     client: './src/client-runner.ts'
   },
+  plugins: [
+    new CopyWebpackPlugin([{
+      from: 'src/main.proto',
+      to: '../dist/main.proto'
+    }])
+  ],
   output: {
     filename: '[name].js',
     path: __dirname + '/build',
@@ -19,5 +26,6 @@ module.exports = merge(baseConfig, {
     host: '0.0.0.0',
     openPage: 'admin.html?debug=1&authKey=SECRET',
     publicPath: '/build/',
+    disableHostCheck: true
   }
 });
