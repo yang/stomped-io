51224692ab8bec88478880643de753c66a3550f2
Yang Zhang <yaaang@gmail.com>
2017-11-23 16:48:49 -0800

    Update tests to pass

--- __tests__/util.ts
+++ __tests__/util.ts
@@ -2,8 +2,8 @@ import * as Common from '../src/common';
 
 describe('objDiff', () => {
   const d = Common.objDiff;
-  it('should return {} for no differences', () => {
-    expect(d({a:0,b:0}, {a:0,b:0})).toEqual({});
+  it('should return null for no differences', () => {
+    expect(d({a:0,b:0}, {a:0,b:0})).toEqual(null);
   })
   it('should return the RHS for only the fields that differ', () => {
     expect(d({a:0,b:0}, {a:0,b:1})).toEqual({b:1});
@@ -12,7 +12,7 @@ describe('objDiff', () => {
     expect(d({a:{a:0,b:0}}, {a:{a:0,b:1}})).toEqual({a:{a:0,b:1}});
   });
   it('should work for arrays', () => {
-    expect(d({a:[0]}, {a:[0]})).toEqual({});
+    expect(d({a:[0]}, {a:[0]})).toEqual(null);
     expect(d({a:[0]}, {a:[1]})).toEqual({a:[1]});
     expect(d({a:[0]}, {a:[]})).toEqual({a:[]});
   });
