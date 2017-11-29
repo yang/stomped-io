3038a60ec04394bf5a36d46d711e9879096c4cce
Yang Zhang <yaaang@gmail.com>
2017-11-29 10:07:15 -0800

    Tweak deploy.bash to minimize broken index.html

--- tools/deploy.bash
+++ tools/deploy.bash
@@ -2,17 +2,20 @@ dest="$1"
 if [[ ! $no_build ]] ; then
     yarn build-prod
 fi
+new_bundle="$(basename dist/bundle*)"
 git archive --format zip -o bounce.zip master
-ssh $dest 'rm bounce/dist/bundle*'
 rsync -ril bounce.zip $dest:
 rsync -ril index.html updates.txt build dist $dest:bounce/
-ssh $dest '
+ssh $dest "
     mkdir -p bounce;
     cd bounce;
+    mv dist/$new_bundle dist/.$new_bundle
+    rm dist/bundle*
+    mv dist/.$new_bundle dist/$new_bundle
     echo A | unzip ../bounce.zip
     rm src/dyn-*.ts
     cp src/dyn.ts src/.dyn-tmp.ts
-    mv src/.dyn-tmp.ts src/dyn-$(date +%Y-%m-%d-%H-%M-%S).ts
+    mv src/.dyn-tmp.ts src/dyn-\$(date +%Y-%m-%d-%H-%M-%S).ts
     . ~/.node/bounce/bin/activate
     yarn install
-'
+"
