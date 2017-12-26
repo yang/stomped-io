129f901e15eb1d7cd847978071afba6c068882eb
Yang Zhang <yaaang@gmail.com>
2017-12-25 16:35:28 -0800

    Collect client network stats

---
+++ tools/collect-stats.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset
+dest=stomped.io
+for host in $dest `cat tools/servers.txt`; do
+    if [[ $host != $dest ]]
+    then host=$host.$dest
+    fi
+    mkdir -p cli-stats/$host/
+    rsync -iz $host:bounce/cli-stats-*.log cli-stats/$host/
+    ssh $host "
+      python -c \"
+import glob, os
+for log in sorted(glob.glob('bounce/cli-stats-*.log'))[:-1]:
+    print 'removing', log
+    os.remove(log)
+\""
+done
