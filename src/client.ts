ab4c2379b6475b87344874f7d1977bc1d6bc9158
Yang Zhang <yaaang@gmail.com>
2017-12-29 21:52:08 -0800

    Manually log backToSplash events to Sentry
    via user reports

--- src/client.ts
+++ src/client.ts
@@ -540,6 +540,7 @@ function initEnts() {
 
   if (initSnap.isDiff) {
     // They switched away tabs, and only now switched back. Chrome force-disables rAF when tab is hidden.
+    setTimeout(() => { throw new Error('initSnap isDiff backToSplash'); }, 0);
     backToSplash();
     return true;
   }
@@ -1367,6 +1368,7 @@ function startGame(name: string, char: string, server: string, onJoin: (socket) 
       cliStats.addBcast(currTime);
       if (timeline.length >= 1024) {
         getLogger('timeline').warn('timeline overrun');
+        setTimeout(() => { throw new Error('timeline overrun backToSplash'); }, 0);
         backToSplash();
       }
       if (timeline.find(b => b.tick == bcast.tick)) return;
